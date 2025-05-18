1a8a2c371f92ddc3e8b5bf458f7c9fdd71914850
Jim Lake <jim@blueskylabs.com>
2025-05-18 09:09:31 -0700

    checking time for consistency

--- test/compare.js
+++ test/compare.js
@@ -1,5 +1,6 @@
 const async = require('async');
 const fs = require('node:fs');
+const { dirname, basename } = require('node:path');
 const FileAttrib = require('../dist/index.js');
 
 const PATH_LIST = [
@@ -23,8 +24,9 @@ async.eachSeries(PATH_LIST, test, () => {
 
 function test(path, done) {
   let a_err;
-  let fs_err;
   let a_result;
+  let b_result;
+  let fs_err;
   let fs_result;
   async.parallel(
     [
@@ -34,6 +36,23 @@ function test(path, done) {
           a_result = result;
           done();
         });
+      },
+      (done) => {
+        const dir = dirname(path);
+        const filename = basename(path).toLowerCase();
+        if (path == dir) {
+          done();
+        } else {
+          FileAttrib.queryDirectory(dir, (err, results) => {
+            b_result = results?.find?.(
+              (r) => r.name.toLowerCase() === filename
+            );
+            if (results && !b_result) {
+              console.log('no b:', dir, filename);
+            }
+            done();
+          });
+        }
       },
       (done) => {
         fs.stat(path, (err, result) => {
@@ -74,7 +93,9 @@ function test(path, done) {
           );
           fail = true;
         }
-        if (_isDiffTime(a_result.ctimeMs, fs_result.ctimeMs)) {
+        if (
+          _isDiffTime(a_result.ctimeMs, fs_result.ctimeMs, b_result?.ctimeMs)
+        ) {
           console.log(
             path,
             'fail ctime mismatch:',
@@ -84,7 +105,9 @@ function test(path, done) {
           );
           fail = true;
         }
-        if (_isDiffTime(a_result.mtimeMs, fs_result.mtimeMs)) {
+        if (
+          _isDiffTime(a_result.mtimeMs, fs_result.mtimeMs, b_result?.mtimeMs)
+        ) {
           console.log(
             path,
             'fail mtime mismatch:',
@@ -110,7 +133,16 @@ function test(path, done) {
     }
   );
 }
-function _isDiffTime(a, b) {
-  const delta = Math.abs(b - a);
-  return delta > 0.01;
+function _isDiffTime(a, fs, b) {
+  const delta_fs = Math.abs(fs - a);
+  if (delta_fs) {
+    console.log('diff_fs:', a, fs);
+  }
+  if (b) {
+    const delta_ab = Math.abs(b - a);
+    if (delta_ab) {
+      console.log('diff_ab:', a, b);
+    }
+  }
+  return delta_fs > 0.01;
 }
